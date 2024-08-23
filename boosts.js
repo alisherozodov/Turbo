86e068546ddb34f5cf0b6e5477b26b420010b79a
codewithphill <166400178+codewithphill@users.noreply.github.com>
2024-08-23 16:34:34 +0500

    Update boosts.js

--- boosts.js
+++ boosts.js
@@ -1,1 +1,28 @@
 
+function loadGame() {
+    if (localStorage.getItem("balance") == null){
+        balance = 0;
+    } else {
+        balance = parseInt(localStorage.getItem("balance"));
+    }
+    if (localStorage.getItem("pps") == null){
+        pps = 1;
+    } else {
+        pps = parseInt(localStorage.getItem("pps"));
+    }
+    if (localStorage.getItem("clickPower") == null){
+        clickPower = 1;
+    } else {
+        clickPower = parseInt(localStorage.getItem("clickPower"));
+    }
+    if (localStorage.getItem("energy") == null){
+        energy = 100;
+    } else {
+        energy = parseInt(localStorage.getItem("energy"));
+    }
+    if (localStorage.getItem("maxEnergy") == null){
+        maxEnergy = 100;
+    } else {
+        maxEnergy = parseInt(localStorage.getItem("maxEnergy"));
+    }
+}
